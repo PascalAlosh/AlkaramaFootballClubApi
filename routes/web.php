d43574281f532e5081e3987c1f17fcb40dde331d
PascalAlosh <pascalalalosh@gmail.com>
2024-04-27 23:39:07 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+  
+//Route::get('lang/home', [LangController::class, 'index']);
+Route::get('lang/change/{lang}', [App\Http\Controllers\languagwController::class, 'change'])->name('changeLang');
+Route::get('/view', [App\Http\Controllers\languagwController::class, 'index']);
+Route::get('/store', [App\Http\Controllers\languagwController::class, 'store']);
+Route::get('/foo', function () {
+
+    Artisan::call('storage:link');
+    echo "hi";
+});
